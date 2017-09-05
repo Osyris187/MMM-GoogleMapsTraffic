9936fe9084048bb449650830adc1ee8038c88e48
Osyris187 <arthur187@web.de>
2017-09-05 20:33:26 +0200

    Added more config options
    - styles array(for layers, streets, POIs, etc)
    - zoom
    - disableDefaultUI
    - enableTraffic

--- MMM-GoogleMapsTraffic.js
+++ MMM-GoogleMapsTraffic.js
@@ -10,8 +10,21 @@
 Module.register("MMM-GoogleMapsTraffic", {
 
 	getDom: function() {
+        //center coordinates for map
         var lat = this.config.lat;
         var lng = this.config.lng;
+
+        //zoom factor, default 10
+        var zoom = (typeof this.config.zoom !== 'undefined') ? this.config.zoom : 10;
+
+        //disable Google's buttons, default true
+        var disableDefaultUI = (typeof this.config.disableDefaultUI !== 'undefined') ? this.config.disableDefaultUI : true;
+
+        //enable traffic layer, default true
+        var enableTraffic = (typeof this.config.enableTraffic !== 'undefined') ? this.config.enableTraffic : true;
+
+        //array with style options
+        var styles = Array.from(this.config.styles);
 
 		var wrapper = document.createElement("div");
         wrapper.setAttribute("id", "map");
@@ -26,15 +39,19 @@ Module.register("MMM-GoogleMapsTraffic", {
 
         script.onload = function () {
             var map = new google.maps.Map(document.getElementById("map"), {
-            	zoom: 13,
+            	zoom: zoom,
             	center: {
             		lat: lat,
             		lng: lng
-            	}
+                },
+                disableDefaultUI: disableDefaultUI,
+                styles: styles
             });
 
-            var trafficLayer = new google.maps.TrafficLayer();
-            trafficLayer.setMap(map);
+            if(enableTraffic){
+                var trafficLayer = new google.maps.TrafficLayer();
+                trafficLayer.setMap(map);
+            }
         };
 
 		return wrapper;
